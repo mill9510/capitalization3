5e60bb33d66a14d164f1a2b7291a1924c37626ec
Mill9510 <mill9510@stud.kea.dk>
2023-09-04 09:55:24 +0200

    js + html

---
+++ capitalization3.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const firstName = "Peter";
+
+const findLetter = firstName.substring(2, 3);
+
+console.log(findLetter);
+
+const upperCase = firstName.substring(2, 3).toUpperCase();
+
+console.log(upperCase);
